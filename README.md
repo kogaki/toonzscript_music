978f8da2a13f20bb00aece8ad1224a52df409284
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2016-08-24 22:19:32 +0900

    :star: generate js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # toonzscript_music
+
+
+A. generate js script with python
+
+```
+python toonzscript_music.py test.wav > output.js
+```
+
+B. load script from **OpenToonz**
--- toonzscript_music.py
+++ toonzscript_music.py
@@ -4,7 +4,8 @@ import scipy.io.wavfile
 import pylab
 import math
 
-FREQ = 44100
+# FREQ = 44100
+FREQ = 48000
 
 parser = argparse.ArgumentParser()
 parser.add_argument("music_file")
@@ -18,19 +19,45 @@ def shortTermEnergy(frame):
   return sum( [ abs(x)**2 for x in frame ] ) / len(frame)
 
 
-def calc_energy(sound, framelen=10, skip=10000):
+def calc_energy(sound, framelen=10000, skip=10000):
     frame = [0 for i in range(framelen)]
     energy = []
     for i, val in enumerate(sound):
         frame.append(val)
         frame.pop(0)
         if(i % skip == 0):
+            # energy.append(math.log(max(shortTermEnergy(frame), 0.001)))
             energy.append(shortTermEnergy(frame))
     return energy
 
 data = scipy.io.wavfile.read( "Test.wav" )[1][:, 0]
 energy = calc_energy(data, skip=int(float(FREQ)/30))
+max_energy = max(energy)
+normalized_energy = [max(0.1, float(e)*3/max_energy) for e in energy]
 
 # pylab.plot(energy)
 # pylab.show()
 
+part = """
+builder = new ImageBuilder();
+builder.add(f_orig, (new Transform).scale({scale}));
+f = builder.image;
+l.setFrame({i}, f);
+"""
+
+sub = ""
+
+for i,en in enumerate(normalized_energy):
+    sub += part.format(i=i+2, scale=en)
+
+
+entire = """
+l = new Level("/Users/keisuke_ogaki/Documents/opentoonz/music/testras.0001.tif");
+f_orig = l.getFrame(1);
+
+{}
+
+l.save("/Users/keisuke_ogaki/Documents/opentoonz/music/testras2.0001.tif");
+"""
+
+print(entire.format(sub))
